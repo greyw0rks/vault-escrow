cb733ff8690091d911b85086db877ca4d0933631
greyw0rks <elchapidave@gmail.com>
2026-05-18 21:32:08 +0100

    Add constants: shared app-wide config values

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = 'VaultSTX';
+export const APP_ICON = '/logo.svg';
+export const SCAN_LIMIT = 50;
+export const STX_DECIMALS = 6;
+export const SUPPORT_EMAIL = 'support@vaultstx.xyz';
